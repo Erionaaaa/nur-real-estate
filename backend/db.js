f10659f8be3a02ae9fcc0e78ba548d4b2fe081d0
Emri Yt <erionakqiku@gmail.com>
2026-03-13 10:57:43 +0100

    update

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: "localhost",
+  port: 3309,
   user: "root",
   password: "Kosova.123",        // ose password yt
   database: "kimidb",
